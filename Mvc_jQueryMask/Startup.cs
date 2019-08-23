b47e11f50964bd64e486647ab135f54a077dbbf9
Wesley Gonçalves Luciano <wesleylg01@gmail.com>
2019-08-22 23:40:33 -0300

    Exemplos de utilização jQuery InptuMask

---
+++ Mvc_jQueryMask/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc_jQueryMask.Startup))]
+namespace Mvc_jQueryMask
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
